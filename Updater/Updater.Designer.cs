6799fca7281aa83f38d9c963790afd69319dc325
Konstantinos-Papanagnou <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2020-10-28 15:48:33 +0200

    Updater Form

---
+++ Updater/Updater.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Updater
+{
+    partial class Updater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updater));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Progresslabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(171, 393);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(653, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Updater.Properties.Resources.Pap_Converter;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(171, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(532, 331);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Progresslabel
+            // 
+            this.Progresslabel.AutoSize = true;
+            this.Progresslabel.Location = new System.Drawing.Point(12, 398);
+            this.Progresslabel.Name = "Progresslabel";
+            this.Progresslabel.Size = new System.Drawing.Size(65, 17);
+            this.Progresslabel.TabIndex = 2;
+            this.Progresslabel.Text = "Progress";
+            // 
+            // Updater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 444);
+            this.Controls.Add(this.Progresslabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Updater";
+            this.Text = "Pap Converter Updater";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Updater_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Progresslabel;
+    }
+}
+
---
+++ Updater/Updater.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Updater
+{
+
+    internal struct VersionData
+    {
+        public string VersionNumber { get; set; }
+        public string UpdateFileUrl { get; set; }
+
+        public VersionData(string VersionNumber, string UpdateFileUrl)
+        {
+            this.VersionNumber = VersionNumber;
+            this.UpdateFileUrl = UpdateFileUrl;
+        }
+    }
+
+    public partial class Updater : Form
+    {
+        bool ProgramKilled = false;
+        bool Updating = false;
+        const string UPDATEURL = "https://github.com/Konstantinos-Papanagnou/Youtube-Downloader-And-Converter/raw/master/setup/Update/update.json";
+        readonly int PID;
+        readonly string LocalDownloadsPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Pap Converter");
+        readonly string VersionFolder; 
+        readonly string VersionPath;
+        const string setup = "https://github.com/Konstantinos-Papanagnou/Youtube-Downloader-And-Converter/raw/master/setup/Pap%20Converter%20Setup.exe";
+        public Updater(int PID)
+        {
+            InitializeComponent();
+            this.PID = PID;
+            progressBar1.Visible = false;
+            Progresslabel.Text = "Checking for Updates...";
+            VersionFolder = System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pap Converter");
+            VersionPath = System.IO.Path.Combine(VersionFolder, "version.txt");
+            Directory.CreateDirectory(VersionFolder);
+            Thread t = new Thread(new ThreadStart(BeginUpdate));
+            t.Start();
+        }
+
+        public void BeginUpdate()
+        {
+            try
+            {
+                if (CheckForUpdates(out VersionData ver_data))
+                {
+                    KillProgram();
+
+                    DownloadAndInstall(ver_data);
+                }
+                else
+                {
+                    Invoke(new Action(() => Progresslabel.Text = "You are up to date!"));
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                }
+            }
+            catch (WebException)
+            {
+                Invoke(new Action(() => Progresslabel.Text = "Unable to establish connection to the internet. Check your network connection or try again later"));
+            }
+        }
+
+        private void DownloadAndInstall(VersionData ver_data)
+        {
+            bool minor = CheckForMinor(ver_data.VersionNumber);
+
+            string remoteUrl = minor ? ver_data.UpdateFileUrl : setup;
+            Invoke(new Action(() => {
+                Progresslabel.Text = "Downloading...";
+                progressBar1.Visible = true;
+            }));
+
+            Directory.CreateDirectory(LocalDownloadsPath);
+            if (File.Exists(Path.Combine(LocalDownloadsPath, "update.exe")))
+                File.Delete(Path.Combine(LocalDownloadsPath, "update.exe"));
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadProgressChanged += (s, e) => { Invoke(new Action(() => { progressBar1.Value = e.ProgressPercentage; })); };
+                client.DownloadFileCompleted += (s, e) => {
+                    Invoke(new Action(() => {
+                        progressBar1.Value = 100;
+                        Progresslabel.Text = "Download Completed";
+                    }));
+
+                    if (Install())
+                    {
+                        Updating = false;
+                        ConfigureUpdate(ver_data.VersionNumber);
+                    }
+
+                };
+                client.DownloadFileAsync(new Uri(remoteUrl), Path.Combine(LocalDownloadsPath, "update.exe"));
+            }
+        }
+
+        private bool CheckForMinor(string versionCode)
+        {
+            string localVer = GetCurrentVersion();
+            return versionCode.Split('.')[1] == localVer.Split('.')[1] || int.Parse(versionCode.Split('.')[1]) - int.Parse(localVer.Split('.')[1]) == 1;
+        }
+
+        private bool Install()
+        {
+            Invoke(new Action(() =>
+            {
+                Progresslabel.Text = "Installing";
+                progressBar1.Visible = false;
+            }));
+
+            const int ERR_CANCELED = 1224;
+            System.Diagnostics.ProcessStartInfo update = new System.Diagnostics.ProcessStartInfo(Path.Combine(LocalDownloadsPath, "update.exe"))
+            {
+                UseShellExecute = true,
+                Verb = "runas"
+            };
+            try
+            {
+                var installation = System.Diagnostics.Process.Start(update);
+                Updating = true;
+                installation.WaitForExit();
+                Invoke(new Action(() =>
+                {
+                    Progresslabel.Text = "Installation Completed!";
+                }));
+                return true;
+            }
+            catch (Win32Exception ex)
+            {
+                if (ex.NativeErrorCode == ERR_CANCELED)
+                    Invoke(new Action(() =>
+                    {
+                        Progresslabel.Text = "Installation Failed. Admin Priviledges Required!";
+                    }));
+                return false;
+            }
+        }
+
+        private void ConfigureUpdate(string VersionData)
+        {
+            Invoke(new Action(() => {
+                Progresslabel.Text = "Processing Triggers...";
+            }));
+            File.WriteAllText(VersionPath, VersionData);
+            Invoke(new Action(() => {
+                Progresslabel.Text = "Firing up Pap Converter!";
+            }));
+
+            StartProgram();
+        }
+
+        private void KillProgram()
+        {
+            if (ProgramKilled)
+                return;
+            if (PID == -1)
+                return;
+            System.Diagnostics.Process.GetProcessById(PID).Kill();
+            ProgramKilled = true;
+        }
+
+        private void StartProgram()
+        {
+            if (ProgramKilled)
+                System.Diagnostics.Process.Start("Pap Converter.exe");
+        }
+
+        private bool CheckForUpdates(out VersionData remoteUrl)
+        {
+            string currentVersion = GetCurrentVersion();
+            VersionData data = GetCloudVersion();
+            remoteUrl = data;
+            return currentVersion != data.VersionNumber; // if current version is not equal to 
+        }
+
+        private string GetCurrentVersion()
+        {
+            Directory.CreateDirectory(VersionFolder);
+            if (File.Exists(VersionPath))
+            {
+                string version = File.ReadAllText(VersionPath);
+                return version;
+            }
+            else
+            {
+                return "1.0.0";
+            }
+        }
+
+        private VersionData GetCloudVersion()
+        {
+            VersionData data; 
+            using (WebClient client = new WebClient())
+                data = JsonConvert.DeserializeObject<VersionData>(client.DownloadString(UPDATEURL));
+            return data;
+        }
+
+        private void Updater_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!Updating)
+            {
+                StartProgram();
+                Application.Exit();
+            }
+            else MessageBox.Show("Pap Converter is Updating. Exiting the Updater during the update process is not recommended...", "Updater.exe");
+        }
+    }
+}
